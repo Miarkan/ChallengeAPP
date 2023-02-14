a6e419175f52bc669954be0599a6c3cedbc38e66
Miarkan <dawid_miarka@wp.pl>
2023-02-14 18:56:49 +0100

    Dzień 4 praca domowa

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿var name = "Ewa";
+bool female = true;
+int age = 33;
+
+if(female && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if(name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");        
+}
+else if(!female && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Niezidentyfikowana osoba");
+}
